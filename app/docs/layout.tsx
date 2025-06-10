053ebbfcf013a6128238b8eb1fdf8f5ea71e29a9
Lasim <hajdas@hajdas.de>
2025-06-10 09:40:22 +0200

    feat: add icon to index.mdx and update layout.tsx sidebar configuration

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -14,6 +14,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       }}
       sidebar={{
         defaultOpenLevel: 1,
+        tabs: false,
       }}
     >
       {children}
